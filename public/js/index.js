e810ca22973a5b811c26fe736d825feb8c74c88b
tfermoso <tomas.fermoso@gmail.com>
2018-08-02 08:25:13 +0200

    inicio interfaz web

--- public/js/index.js
+++ public/js/index.js
@@ -5,9 +5,15 @@ $.ajax({
     dataType: "json",
     success: function (response) {
         console.log(response); 
-               
+        for (const producto in response.productos) {
+            if (response.productos.hasOwnProperty(producto)) {
+                var div=`<div class='ranura ${producto}'></div>`;
+                $('.productos').append(div);
+                
+            }
+        }     
         for (let i = 0; i < response["ranuras vacias"]; i++) {
-            $('.productos').append("<div class='espiral'></div>");
+            $('.productos').append("<div class='ranura espiral'></div>");
             
         }
     }
