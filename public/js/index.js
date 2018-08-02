7c86694f24d48eb1877bfaf7a304a26decf87e42
tfermoso <tomas.fermoso@gmail.com>
2018-08-02 13:55:16 +0200

    comprar producto

--- public/js/index.js
+++ public/js/index.js
@@ -1,33 +1,95 @@
+var monedasInsertadas = [];
+var productoSeleccionado = "";
 
 $.ajax({
     type: "get",
     url: "/",
     dataType: "json",
     success: function (response) {
-        console.log(response); 
+        console.log(response);
+
         for (const producto in response.productos) {
             if (response.productos.hasOwnProperty(producto)) {
-                var div=`<div id='${producto}' class='ranura ${producto}'></div>`;
+                var div = `<div id='${producto}' class='ranura ${producto}'></div>`;
                 $('.productos').append(div);
-                
             }
-        }     
+        }
         for (let i = 0; i < response["ranuras vacias"]; i++) {
             $('.productos').append("<div class='ranura espiral'></div>");
-            
         }
         eventosRanuras();
     }
 });
 
 
-function eventosRanuras(){
-    $('.ranura').click(ev=>{
-        console.log(ev);
-        alert("ranura pulsada");
+function eventosRanuras() {
+    $('.ranura').click(ev => {
+        productoSeleccionado = "";
+        if (ev.currentTarget.id == "") {
+            alert("ranura vacia");
+        } else {
+            console.log("Producto seleccionado: " + ev.currentTarget.id);
+            var el = `#${ev.currentTarget.id}`;
+            if ($(el).hasClass('ranuraSel')) {
+                $('.ranura').removeClass('ranuraSel');
+                $('.importe').html("0.00")
+            } else {
+                productoSeleccionado = ev.currentTarget.id;
+                $('.ranura').removeClass('ranuraSel');
+                $(el).addClass('ranuraSel');
+                $.ajax({
+                    type: "post",
+                    url: "/precio",
+                    data: { "producto": productoSeleccionado },
+                    dataType: "json",
+                    success: function (response) {
+                        console.log(response);
+                        $('.importe').html(response);
+                    }
+                });
+            }
+        }
     })
 }
 
 
+$('.monedas img').click((ev) => {
+    var precioProducto = parseFloat($('.importe').html());
+    var credito = parseFloat($('.credito').html());
+    credito += parseFloat(ev.currentTarget.id);
+    monedasInsertadas.push(parseFloat(ev.currentTarget.id));
+    console.log(monedasInsertadas)
+    $('.credito').html(credito.toFixed(2));
+    if (credito >= precioProducto && productoSeleccionado != "") {
+        comprarProducto(productoSeleccionado,monedasInsertadas);
+    }
+
+})
+
+function comprarProducto(producto,credito) {    
+        $.ajax({
+            type: "post",
+            url: "/comprar",
+            data: {
+                "producto": producto,
+                "credito": JSON.stringify(credito)
+            },
+            dataType: "json",
+            success: function (response) {
+                console.log(response);
+                var cambio = 0;
+                response.result.cambio.forEach(ele => {
+                    cambio += parseFloat(ele);
+                });
+                $('.credito').html(cambio.toFixed(2));
+                $('.importe').html("0.00");
+
+                response.result.mensaje.forEach(msg => {
+                    $('#avisos').append(msg+"</br>");
+                });
+            }
+        });
+  
+}
 
 
--- server.js
+++ server.js
@@ -1,14 +1,37 @@
 const Expendedora = require('./Expendedora');
 const express = require('express');
-
+var bodyParser = require('body-parser');
 
 const app = express();
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+
 var maquina = new Expendedora("máquina 1", 18, 10, "monedas.json");
 maquina.cargarProductosDesdeFichero("productos.json");
 
 app.get('/', (req, res) => res.send(
     JSON.stringify(maquina.leerAvisos())
 ))
+
+app.post('/precio',(req,res)=>{
+    let precio=maquina.seleccionarProducto(req.body.producto);    
+    res.send(JSON.stringify(precio));
+})
+
+app.post('/comprar',(req,res)=>{  
+    console.log(req.body);
+    var result=maquina.comprarProducto(req.body.producto,JSON.parse(req.body.credito));
+    var avisos=maquina.leerAvisos();
+    var respuesta={
+        "result": result,
+        "aviso":avisos
+    }
+    res.send(JSON.stringify(respuesta));
+})
 
 app.use(express.static('public'))
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
